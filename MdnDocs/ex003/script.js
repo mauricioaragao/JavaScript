bb2dd7c0f2f6a923f47f8c6b1630e1f81c4f4f90
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-03-28 21:28:35 -0300

    Atualização.

--- MdnDocs/ex003/script.js
+++ MdnDocs/ex003/script.js
@@ -12,3 +12,42 @@ var campoPalpite = document.querySelector('.campoPalpite');
 
 var contagemPalpites = 1;
 var botaoReinicio;
+
+/*
+function conferirPalpite() {
+    alert('Eu sou um placeholder');
+}
+*/
+
+function conferirPalpite() {
+    var palpiteUsuario = Number(campoPalpite.value);
+    //Primeira tentativa do jogador.
+    if (contagemPalpites === 1) {
+        palpites.textContent = 'Palpites anteriores: ';
+    }
+
+    palpites.textContent += palpiteUsuario + ' ';
+
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = 'Parabéns! Você acertou!';
+        ultimoResultado.style.backgroundColor = 'green';
+        baixoOuAlto.textContent = '';
+        configFimDeJogo();
+    } else if (contagemPalpites === 10) {
+        ultimoResultado.textContent = 'FIM DE JOGO!!!'
+        baixoOuAlto.textContent = '';
+        configFimDeJogo();
+    } else {
+        ultimoResultado.textContent = 'Errado!'
+        ultimoResultado.style.backgroundColor = 'red';
+        if(palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = 'Seu palpite está muito baixo!'
+        } else if(palpiteUsuario > numeroAleatorio) {
+            baixoOuAlto.textContent = 'Seu número está muito alto!'
+        }
+    }
+
+    contagemPalpites++;
+    campoPalpite.value = '';
+    campoPalpite.focus();
+}
