5a8709f3625fc1e80a2f7275af66ae29e6d12a9d
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-03-30 12:16:40 -0300

    Atualizaçao

--- MdnDocs/ex003/script.js
+++ MdnDocs/ex003/script.js
@@ -7,12 +7,13 @@ var palpites = document.querySelector('.palpites');
 var ultimoResultado = document.querySelector('.ultimoResultado');
 var baixoOuAlto = document.querySelector('.baixoOuAlto');
 
+
 var envioPalpite = document.querySelector('.envioPalpite');
 var campoPalpite = document.querySelector('.campoPalpite');
 
 var contagemPalpites = 1;
 var botaoReinicio;
-campoPalpite.focus();
+campoPalpite.focus();   
 
 /*
 function conferirPalpite() {
