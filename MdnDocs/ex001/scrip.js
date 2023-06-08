d75d9cbd1df192fe78f3a6c1d75aa1ac2943545f
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-06-07 22:57:23 -0300

    Delete MdnDocs directory

--- MdnDocs/ex001/scrip.js
+++
@@ -1,8 +0,0 @@
-const para = document.querySelector('p');
-
-para.addEventListener('click', atualizarNome);
-
-function atualizarNome() {
-  var nome = prompt('Insira um novo nome');
-  para.textContent = 'Jogador 1: ' + nome;
-}
