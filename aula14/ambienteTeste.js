496a14fd4c1937d25d10163c11885835756d45cb
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-06-17 00:28:00 -0300

    Depuração

---
+++ aula14/ambienteTeste.js
@@ -0,0 +1,13 @@
+/*
+let d = 1
+while (d <= 10) {
+    console.log(d)
+    d++
+}
+*/
+
+console.log('Vai começar...')
+for (let d = 1; d <= 10; d++) {
+    console.log(d)
+}
+console.log('Terminou!')
