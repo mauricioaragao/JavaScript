3afd378c33b494a36b3cab3c11c505b7c10ac7c9
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-06-17 00:06:48 -0300

    Estrutura de repetição

---
+++ aula13/ambienteTeste.js
@@ -0,0 +1,29 @@
+//Uso de do while - teste lógico do final
+let b = 1
+do {
+    console.log(`Passo ${b}`)
+    b++
+} while (b <= 7)
+
+
+
+//Uso de while - teste lógico do início
+//Mais utilizada
+/*
+let c = 1
+while (c <= 5) {
+    console.log(`Conte ${c}`)
+    c++
+}
+*/
+
+
+
+/*
+Menos usada.
+console.log('Beleza')
+console.log('Beleza')
+console.log('Beleza')
+console.log('Beleza')
+console.log('Beleza')
+*/
