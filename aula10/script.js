70ecd9a226160f8ab4f73a3d5a9ea73df3dd4df4
Maurício Aragão <122069164+mauricioaragao@users.noreply.github.com>
2023-07-22 22:03:58 -0300

    Atualização.

--- aula10/script.js
+++ aula10/script.js
@@ -7,12 +7,22 @@ a.addEventListener('mousemove', mover)
 function clicar() {
     a.innerText = 'Clicou!'
     a.style.background = 'blue'
+    a.style.cursor = 'progress'
 }
+
 function entrar() {
     a.innerText = 'Entrou!'
     a.style.background = 'gray'
+    a.style.cursor = 'pointer'
 }
+
 function sair() {
     a.innerText = 'Saiu!'
     a.style.background = 'green'
+}
+
+function mover() {
+    a.innerText = "Movendo!"
+    a.style.background = 'purple'
+    a.style.cursor = 'pointer'
 }
